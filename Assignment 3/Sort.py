184362f28ff053d6828660cd9faeacfffa4930f8
josmoor <josmoor@iu.edu>
2023-09-24 09:39:08 -0900

    Updated Comments

--- Assignment 3/Sort.py
+++ Assignment 3/Sort.py
@@ -8,6 +8,13 @@
 # Variables
 array = [];
 
+'''
+    Type: Function
+    Name: getUserInput
+    Return: Void
+    Description: Gets the user's input. in total, asks the user to input ten numerical values in any order. If the
+                 inputted value is not a numerical value, then the loop exists.
+'''
 def getUserInput():
     userInput = input("Enter a number (Use non-number to stop): ");
     if(userInput.isdigit()):
@@ -19,6 +26,14 @@ def getUserInput():
         if(userInput.isdigit()):
             array.append(int(userInput));
 
+'''
+    Type: Function
+    Name: sort
+    Return: Void
+    Description: Sorts through the array of user inputted values. The sorting method is selection-sort, meaning it looks at each
+                 value past a current value in the array, to check if the value is less than the current value. This sorting method
+                 takes a complexity of O(n).
+'''
 def sort():
     for i in range(len(array)):
         for j in range(i + 1, len(array)):
@@ -27,6 +42,7 @@ def sort():
                 array[i] = array[j];
                 array[j] = tmp;
 
+# Main call functions
 getUserInput();
 sort();
 print(array);
